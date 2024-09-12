3f5849d69725265909933f116677c82c3efc146e
lazyihui <chengbaozi6@qq.com>
2024-09-12 17:15:15 +0800

    establish

---
+++ Assets/Rescures_Scripts/Main.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+
+    void Update()
+    {
+
+    }
+}
